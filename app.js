71019484134d59f0aaaa9cfaaa2ca51e24dc651d
Chase Hawthorne <ch73168@gmail.com>
2016-10-09 05:32:46 +0100

    Hellojs stuff

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
 var express = require("express");
 var app = express();
 
+var clientID = "275496876481-an8bs3mhbk0co88l85bkgn3pr5sdvoda.apps.googleusercontent.com";
+var clientSecret = "D9Effw443ZZV2fR6hiY0-hvK";
+
 app.use(express.static("public"));
 
-app.listen(8888);
+app.get('/login', function(req, res){
+  
+});
+
+app.listen(8888, function(){
+  console.log("Server listening on port 8888...");
+});
